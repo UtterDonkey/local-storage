b0ea7a351aafa9a3e7f81ef3e5dc7834ef057971
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2021-05-04 19:46:17 +0100

    Add files via upload

---
+++ ex.js
@@ -0,0 +1,61 @@
+
+        headers: { 
+         'Access-Control-Allow-Origin': '*'
+        }
+
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/localforage/1.7.3/localforage.min.js')
+
+class LocalStorage {
+  constructor () {}
+
+  getInfo () {
+    return {
+      id: 'localstorage',
+      name: 'LocalStorage',
+
+      blocks: [
+        {
+          opcode: 'readLocalStorage',
+
+          blockType: Scratch.BlockType.REPORTER,
+
+          text: 'get [NAME]',
+          arguments: {
+            NAME: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'highscore'
+            }
+          }
+        },
+        {
+          opcode: 'setLocalStorage',
+
+          blockType: Scratch.BlockType.COMMAND,
+
+          text: 'set [NAME] to [VALUE]',
+          arguments: {
+            NAME: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'highscore'
+            },
+            VALUE: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: '12'
+            }
+          }
+        }
+      ]
+    }
+  }
+
+  readLocalStorage ({ NAME }) {
+    return localforage.getItem(NAME)
+      .then(value => value || '')
+  }
+
+  setLocalStorage ({ NAME, VALUE }) {
+    return localforage.setItem(NAME, VALUE)
+  }
+}
+
+Scratch.extensions.register(new LocalStorage())
